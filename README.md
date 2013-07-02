265237a798ff7f2918032d21feadf40188020f9a
Nate Beatty <nate.beatty@gmail.com>
2013-07-02 09:58:07 -0400

    Namespace P created

---
+++ README.md
@@ -0,0 +1,4 @@
+Pyramid
+===
+
+Clone, fire up your favorite webserver, and check out the demo.
--- js/pyramid.js
+++ js/pyramid.js
@@ -11,138 +11,145 @@
  */
 
 "use strict";
+var P = {
+	pyramid: { // Keep track of some stats about the pyramid
+		maxpop: 0,
+		maxpx: 175,
+		leftVals: [],
+		rightVals: []
+	},
 
-var pyramid = { // Keep track of some stats about the pyramid
-	maxpop: 0,
-	maxpx: 175,
-	leftVals: [],
-	rightVals: []
-};
+	/* 
+	 * Initializes the pyramid by appending the appropriate
+	 * html to the inside of the div of class 'pyramid'. Also gets the
+	 * pyramid ready to be redrawn with values corresponding to populations.
+	 *
+	 * @param maxpop (int) Specifies the maximum value that can be
+	 * displayed in the pyramid. This should be the population value that
+	 * a bar stretching all of the way across its allotted width would
+	 * represent. The value of this parameter is used to calculate bar widths
+	 * each time the pyramid is redrawn.
+	 */
+	initPyramid: function(maxpop) {
+		console.log('Building the pyramid. Max value: ' + maxpop);
 
-/* 
- * Initializes the pyramid by appending the appropriate
- * html to the inside of the div of class 'pyramid'. Also gets the
- * pyramid ready to be redrawn with values corresponding to populations.
- *
- * @param maxpop (int) Specifies the maximum value that can be
- * displayed in the pyramid. This should be the population value that
- * a bar stretching all of the way across its allotted width would
- * represent. The value of this parameter is used to calculate bar widths
- * each time the pyramid is redrawn.
- */
-var initPyramid = function(maxpop) {
-	console.log('Building the pyramid. Max value: ' + maxpop);
+		$('.pyramid div').html(''); // start fresh - clear any html in the pyramid
 
-	$('.pyramid div').html(''); // start fresh - clear any html in the pyramid
+		// Build the left bars
+		var lbars = [];
+		for (var i = 0; i < 20; i++) {
+			var barID = 'lbar' + i;
+			lbars[i] = '<div class="bar"><div class="lbar" id="' + barID + '"></div></div>';
+		};
+		var lbarDivs = lbars.join('');
 
-	// Build the left bars
-	var lbars = [];
-	for (var i = 0; i < 20; i++) {
-		var barID = 'lbar' + i;
-		lbars[i] = '<div class="bar"><div class="lbar" id="' + barID + '"></div></div>';
-	};
-	var lbarDivs = lbars.join('');
+		// Build the right bars
+		var rbars = [];
+		for (var i = 0; i < 20; i++) {
+			var barID = 'rbar' + i;
+			rbars[i] = '<div class="bar"><div class="rbar" id="' + barID + '"></div></div>';
+		};
+		var rbarDivs = rbars.join('');
 
-	// Build the right bars
-	var rbars = [];
-	for (var i = 0; i < 20; i++) {
-		var barID = 'rbar' + i;
-		rbars[i] = '<div class="bar"><div class="rbar" id="' + barID + '"></div></div>';
-	};
-	var rbarDivs = rbars.join('');
+		$('.pyramid').append('<div id="container"></div>');
+		$('#container').append('<div id="lcontainer">' + lbarDivs + '</div>');
+		$('#container').append('<div id="rcontainer">' + rbarDivs + '</div>');
+		$('.lbar').resizable({
+			handles: 'w',
+			maxWidth: P.pyramid.maxpx,
+			minWidth: 0
+		});
+		$('.rbar').resizable({
+			handles: 'e',
+			maxWidth: P.pyramid.maxpx,
+			minWidth: 0
+		});
 
-	$('.pyramid').append('<div id="container"></div>');
-	$('#container').append('<div id="lcontainer">' + lbarDivs + '</div>');
-	$('#container').append('<div id="rcontainer">' + rbarDivs + '</div>');
-	$('.lbar').resizable({
-		handles: 'w',
-		maxWidth: pyramid.maxpx,
-		minWidth: 0
-	});
-	$('.rbar').resizable({
-		handles: 'e',
-		maxWidth: pyramid.maxpx,
-		minWidth: 0
-	});
+		P.pyramid['maxpop'] = maxpop;
+		P.pyramid['maxpx'] = $('.bar').first().width();
 
-	pyramid['maxpop'] = maxpop;
-	pyramid['maxpx'] = $('.bar').first().width();
+		// Set listeners to respond to events within the pyramid
+		$('.lbar,.rbar').resize(function() {
+			P.barWasDragged($(this));
+		});
 
-	// Set listeners to respond to events within the pyramid
-	$('.lbar,.rbar').resize(function(){
-		barWasDragged($(this));
-	});
+		// Set popups on hover for the bars using Tipsy
+		$('.lbar,.rbar').tipsy({
+			title: 'id',
+			fade: true,
+			gravity: 's'
+		});
+	},
 
-	// Set popups on hover for the bars using Tipsy
-	$('.lbar,rbar').tipsy({title: 'id', fade: true, gravity: 'se'});
-};
+	/* Resizes the initialized pyramid with values given in the parameters.
+	 *
+	 * @param leftVals (array of 20 int values) An array of values, one for
+	 * each bar on the left side of the pyramid. These should be population values.
+	 *
+	 * @param rightVals (array of 20 int values) An array of values, one for
+	 * each bar on the right side of the pyramid. These should be population values.
+	 */
+	drawPyramid: function(leftVals, rightVals) {
+		if (typeof leftVals != 'undefined' && typeof rightVals != 'undefined') {
+			P.pyramid['leftVals'] = leftVals;
+			P.pyramid['rightVals'] = rightVals;
+		}
 
-/* Resizes the initialized pyramid with values given in the parameters.
- *
- * @param leftVals (array of 20 int values) An array of values, one for
- * each bar on the left side of the pyramid. These should be population values.
- *
- * @param rightVals (array of 20 int values) An array of values, one for
- * each bar on the right side of the pyramid. These should be population values.
- */
-var drawPyramid = function(leftVals, rightVals) {
-	if (typeof leftVals != 'undefined' && typeof rightVals != 'undefined') {
-		pyramid['leftVals'] = leftVals;
-		pyramid['rightVals'] = rightVals;
+		if (P.pyramid.leftVals.length != 20 || P.pyramid.rightVals.length != 20) {
+			console.log('In pyramid.js: drawPyramid() parameter values must be arrays of length 20.')
+			return;
+		}
+
+		$('.lbar').each(function(index) {
+			var newBarWidth = P.popToPixels(P.pyramid.leftVals[index]);
+			if (newBarWidth > P.pyramid.maxpx) {
+				console.log('Left bar ' + index + ' value overflow.');
+			}
+			$(this).width(newBarWidth);
+			var leftoffset = P.pyramid.maxpx - newBarWidth;
+			$(this).css('left', leftoffset); // To right align the left bars
+		});
+		$('.rbar').each(function(index) {
+			var newBarWidth = P.popToPixels(P.pyramid.rightVals[index]);
+			if (newBarWidth > P.pyramid.maxpx) {
+				console.log('Right bar ' + index + ' value overflow.');
+			}
+			$(this).width(newBarWidth);
+		});
+	},
+
+	barWasDragged: function(e) {
+		var elementID = e.attr('id');
+		var isLeftBar = (elementID[0] == 'l') ? true : false;
+		var barIndex = parseInt(elementID.substring(4, elementID.length));
+
+		// For testing
+		var side = (isLeftBar) ? 'left' : 'right';
+		console.log('Width of ' + side + ' bar ' + barIndex + ' changed to: ' + e.width() + ' px = ' + P.pixelsToPop(e.width()) + ' people.');
+	},
+
+	/***********************
+	 **  Utility Methods  **
+	 ***********************/
+
+	pixelsToPop: function(px) {
+		return Math.round((px / P.pyramid.maxpx) * P.pyramid.maxpop);
+	},
+
+	popToPixels: function(pop) {
+		return Math.round((pop / P.pyramid.maxpop) * P.pyramid.maxpx);
 	}
 
-	if (pyramid.leftVals.length != 20 || pyramid.rightVals.length != 20) {
-		console.log('In pyramid.js: drawPyramid() parameter values must be arrays of length 20.')
-		return;
-	}
+	
+}
 
-	$('.lbar').each(function(index) {
-		var newBarWidth = popToPixels(pyramid.leftVals[index]);
-		if (newBarWidth > pyramid.maxpx) {
-			console.log('Left bar ' + index + ' value overflow.');
-		}
-		$(this).width(newBarWidth);
-		var leftoffset = pyramid.maxpx - newBarWidth;
-		$(this).css('left', leftoffset); // To right align the left bars
-	});
-	$('.rbar').each(function(index) {
-		var newBarWidth = popToPixels(pyramid.rightVals[index]);
-		if (newBarWidth > pyramid.maxpx) {
-			console.log('Right bar ' + index + ' value overflow.');
-		}
-		$(this).width(newBarWidth);
-	});
-};
-
-var barWasDragged = function(e) {
-	var elementID = e.attr('id');
-	var isLeftBar = (elementID[0] == 'l') ? true : false;
-	var barIndex = parseInt(elementID.substring(4, elementID.length));
-
-	// For testing
-	var side = (isLeftBar) ? 'left' : 'right';
-	console.log('Width of '+side+' bar '+barIndex+' changed to: '+e.width()+'px='+pixelsToPop(e.width())+'ppl.');
-};
-
-/**********************
-**  Utility Methods  **
-**********************/
-
-function pixelsToPop(px) {
-	return Math.round((px / pyramid.maxpx) * pyramid.maxpop);
-};
-
-function popToPixels(pop) {
-	return Math.round((pop / pyramid.maxpop) * pyramid.maxpx);
-};
-
-/***************************
-**  Execute on DOM Ready  **
-***************************/
+/****************************
+ **  Execute on DOM Ready  **
+ ****************************/
 
 $(document).ready(function() {
 
-	initPyramid(2000000);
+	P.initPyramid(2000000);
 
 	var randomList = function() { // For testing
 		var list = [];
@@ -152,6 +159,6 @@ $(document).ready(function() {
 		return list;
 	};
 
-	drawPyramid(randomList(), randomList());
+	P.drawPyramid(randomList(), randomList());
 
 });
