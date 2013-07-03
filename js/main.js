a44876ffeafd8935d4b082eb51618cec8bef3b1b
Nate Beatty <nate.beatty@gmail.com>
2013-07-03 09:16:43 -0400

    Num of Bars

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	// Generates random populations between 0 and 2000000
 	var randomList = function() { // For Testing
 		var list = [];
-		for (var i = 1; i <= 20; i++) {
+		for (var i = 1; i <= 21; i++) {
 			list.push(Math.floor(Math.random() * 1900000) + 1);
 		}
 		return list;
